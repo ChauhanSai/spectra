521d415cf7dd9d799371a8dd6ab90f785e46b223
emraany <emraany1220@gmail.com>
2026-02-14 21:00:57 -0600

    Delete src/test_saliency.py

--- src/test_saliency.py
+++
@@ -1,7 +0,0 @@
-from utils.saliency import get_salient_cells
-
-img_path = "data/coco/train2017/000000000064.jpg"
-most, least = get_salient_cells(img_path)
-
-print("Most salient:", most)
-print("Least salient:", least)
